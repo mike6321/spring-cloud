d213862fddc25dc77520a3fefbcc2901a25e3a78
mike6321 <mike6321@naver.com>
2021-11-15 09:34:08 +0900

    (first-service) Netflix Zuul - 프로젝트 생성

---
+++ second-service/src/main/java/me/choi/secondservice/SecondServiceApplication.java
@@ -0,0 +1,13 @@
+package me.choi.secondservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SecondServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SecondServiceApplication.class, args);
+    }
+
+}
