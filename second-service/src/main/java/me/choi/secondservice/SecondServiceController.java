c134ad5ed2cbf2b0d6d9e6f67f6b4fcb5f5a0a91
mike6321 <mike6321@naver.com>
2021-11-15 09:34:42 +0900

    (second-service) Netflix Zuul - 프로젝트 생성

--- second-service/src/main/java/me/choi/secondservice/SecondServiceController.java
+++ second-service/src/main/java/me/choi/secondservice/SecondServiceController.java
@@ -1,2 +1,15 @@
-package me.choi.secondservice;public class SecondServiceController {
+package me.choi.secondservice;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/")
+public class SecondServiceController {
+
+    @GetMapping("/welcome")
+    public String welcome() {
+        return "Welcome to the Second service";
+    }
 }
