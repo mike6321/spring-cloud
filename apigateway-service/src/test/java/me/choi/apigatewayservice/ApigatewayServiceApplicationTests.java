abb20c72b4deecb886afc0b66792883fc043f854
mike6321 <mike6321@naver.com>
2021-11-16 20:11:55 +0900

    (apigateway-service) Spring Cloud Gateway - 프로젝트 생성

---
+++ apigateway-service/src/test/java/me/choi/apigatewayservice/ApigatewayServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package me.choi.apigatewayservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApigatewayServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
