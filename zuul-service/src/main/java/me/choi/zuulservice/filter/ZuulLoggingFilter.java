e3edbe45aa30b5174b2794e58953a4872f09bde8
mike6321 <mike6321@naver.com>
2021-11-16 18:56:16 +0900

    (zuul-service) Netflix Zuul - Filter 적용

---
+++ zuul-service/src/main/java/me/choi/zuulservice/filter/ZuulLoggingFilter.java
@@ -0,0 +1,44 @@
+package me.choi.zuulservice.filter;
+
+import com.netflix.zuul.ZuulFilter;
+import com.netflix.zuul.context.RequestContext;
+import com.netflix.zuul.exception.ZuulException;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Component;
+
+import javax.servlet.http.HttpServletRequest;
+
+@Slf4j
+@Component
+public class ZuulLoggingFilter extends ZuulFilter {
+
+    @Override
+    public Object run() throws ZuulException {
+        log.info("***************print logs : ***************");
+
+        RequestContext currentContext = RequestContext.getCurrentContext();
+        HttpServletRequest request = currentContext.getRequest();
+
+        log.info("{} ", request.getRequestURI());
+
+        log.info("***************print logs : ***************");
+
+        return null;
+    }
+
+    @Override
+    public String filterType() {
+        return "pre";
+    }
+
+    @Override
+    public int filterOrder() {
+        return 1;
+    }
+
+    @Override
+    public boolean shouldFilter() {
+        return true;
+    }
+
+}
