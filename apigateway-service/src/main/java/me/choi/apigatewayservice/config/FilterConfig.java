9e33b27b3c052654040753e2f249ef25dc65163d
mike6321 <mike6321@naver.com>
2021-11-20 23:01:44 +0900

    remove not using import

--- apigateway-service/src/main/java/me/choi/apigatewayservice/config/FilterConfig.java
+++ apigateway-service/src/main/java/me/choi/apigatewayservice/config/FilterConfig.java
@@ -2,13 +2,11 @@ package me.choi.apigatewayservice.config;
 
 import org.springframework.cloud.gateway.route.RouteLocator;
 import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
 
-@Configuration
+//@Configuration
 public class FilterConfig {
 
-    @Bean
+//    @Bean
     public RouteLocator gatewayRoutes(RouteLocatorBuilder builder) {
 
         return builder.routes()
