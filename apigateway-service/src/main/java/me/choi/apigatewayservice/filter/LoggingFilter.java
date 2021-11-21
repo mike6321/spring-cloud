ebbac06c6c34f59d0a057fcb061e8088281c0ee6
mike6321 <mike6321@naver.com>
2021-11-21 18:50:24 +0900

    Spring Cloud Gateway - Logging Filter

---
+++ apigateway-service/src/main/java/me/choi/apigatewayservice/filter/LoggingFilter.java
@@ -0,0 +1,54 @@
+package me.choi.apigatewayservice.filter;
+
+import lombok.Data;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.cloud.gateway.filter.GatewayFilter;
+import org.springframework.cloud.gateway.filter.OrderedGatewayFilter;
+import org.springframework.cloud.gateway.filter.factory.AbstractGatewayFilterFactory;
+import org.springframework.core.Ordered;
+import org.springframework.http.server.reactive.ServerHttpRequest;
+import org.springframework.http.server.reactive.ServerHttpResponse;
+import org.springframework.stereotype.Component;
+import reactor.core.publisher.Mono;
+
+@Slf4j
+@Component
+public class LoggingFilter extends AbstractGatewayFilterFactory<LoggingFilter.Config> {
+
+    public LoggingFilter() {
+        super(Config.class);
+    }
+
+    @Override
+    public GatewayFilter apply(Config config) {
+        GatewayFilter filter = new OrderedGatewayFilter((exchange, chain) -> {
+            ServerHttpRequest request = exchange.getRequest();
+            ServerHttpResponse response = exchange.getResponse();
+
+            log.info("Logging Filter baseMessage: {}", config.getBaseMessage());
+            if (config.isPreLogger()) {
+                log.info("Logging Pre Filter: request id -> {}", request.getId());
+            }
+
+            // Custom Post Filter
+            return chain.filter(exchange).then(Mono.fromRunnable(() -> {
+                if (config.isPostLogger()) {
+                    log.info("Logging Post Filter: request id -> {}", response.getStatusCode());
+                }
+
+                log.info("Logging Post Filter: response code -> {}", response.getStatusCode());
+            }));
+        }, Ordered.HIGHEST_PRECEDENCE);
+
+        return filter;
+    }
+
+    @Data
+    public static class Config {
+        // put the configuration properties
+        private String baseMessage;
+        private boolean preLogger;
+        private boolean postLogger;
+    }
+
+}
