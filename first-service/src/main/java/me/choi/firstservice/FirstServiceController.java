2fc58182248be38ccc5b71193f3e088f55f4a11c
최준우 <mike6321@nhnsoft.com>
2021-11-22 11:17:10 +0900

    Spring Cloud Gateway - Load Balancer 2

---
+++ first-service/src/main/java/me/choi/firstservice/FirstServiceController.java
@@ -0,0 +1,39 @@
+package me.choi.firstservice;
+
+import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.core.env.Environment;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestHeader;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.servlet.http.HttpServletRequest;
+
+@Slf4j
+@AllArgsConstructor
+@RestController
+@RequestMapping("/first-service")
+public class FirstServiceController {
+
+    private final Environment environment;
+
+    @GetMapping("/welcome")
+    public String welcome() {
+        return "Welcome to the First service";
+    }
+
+    @GetMapping("/message")
+    public String message(@RequestHeader("first-request") String header) {
+        log.info(header);
+
+        return "Hello World in First Service.";
+    }
+
+    @GetMapping("/check")
+    public String check(HttpServletRequest request) {
+        log.info("Server port={}", request.getServerPort());
+        return String.format("Hi, there. This is a message from First Service on Port %s", environment.getProperty("local.server.port"));
+    }
+
+}
