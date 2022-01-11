668303fd19e0cf68708c5ed7c539e33cbf9ab3f6
mike6321 <mike6321@naver.com>
2022-01-11 09:06:35 +0900

    apply to api-gateway config

--- apigateway-service/src/main/java/me/choi/apigatewayservice/ApigatewayServiceApplication.java
+++ apigateway-service/src/main/java/me/choi/apigatewayservice/ApigatewayServiceApplication.java
@@ -1,13 +1,21 @@
 package me.choi.apigatewayservice;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.actuate.trace.http.HttpTraceRepository;
+import org.springframework.boot.actuate.trace.http.InMemoryHttpTraceRepository;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class ApigatewayServiceApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(ApigatewayServiceApplication.class, args);
+    }
+
+    @Bean
+    public HttpTraceRepository httpTraceRepository() {
+        return new InMemoryHttpTraceRepository();
     }
 
 }
